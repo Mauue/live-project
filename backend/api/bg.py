aa5989e02502aaef7731484ffce93b27235c23b2
LinJie1119 <305946294@qq.com>
2020-03-15 15:09:32 +0800

    update bg.py

--- backend/api/bg.py
+++ backend/api/bg.py
@@ -2,7 +2,24 @@ from api import api, make_response
 from flask import request
 
 
-@api.route('/bg/set_order')
+@api.route('/bg/set_order', methods=['POST'])
 def set_order():
-    pass
+    start_time = request.form.get('start_time')
+    end_time = request.form.get('end_time')
+    total = request.form.get('total')
+    order_max = request.form.get('order_max')
+
+    if start_time is None:
+        return make_response(msg="未设置开始日期")
+    if end_time is None:
+        return make_response(msg="未设置结束日期")
+    if total is None:
+        return make_response(msg="未设置总量")
+    if order_max is None:
+        return make_response(msg="未设置预约数量")
+
+    db, cursor = get_db()
+    cursor.execute("INSERT INTO order_set(start_time, end_time, total, order_max) "
+                   "values (%s, %s, %s, %s)", (start_time, end_time, total, order_max))
+
 
