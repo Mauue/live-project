a0cf0f916484a1c71af3fc2fe50d6875fcbd65a3
heng1999 <1344434624@qq.com>
2020-03-15 22:42:43 +0800

    完善客服界面

--- frontend/src/components/Service.vue
+++ frontend/src/components/Service.vue
@@ -1,7 +1,26 @@
 <template>
    <div id="app">
-		<h3>请联系人工客服 微信：<a href="#">XXXXX</a></h3>
-        <h4>人工客服上班时间:每周一到周五8:00——20:00</h4>
+       <img src="../assets/common/客服.png" />
+		<em>请联系人工客服 微信：<a href="#">XXXXX</a></em><br /><br /><br />
+        <p id="add">*人工客服上班时间:每周一到周五8:00——20:00</p>
 	</div>
 </template>
 
+<style>
+img{
+    margin-top:100px;
+    width:100px;
+    height:100px;
+    margin-left:140px;
+}
+em{
+    padding-top:10px;
+    text-decoration: none;
+    font-size: 20px;
+    font-style: normal;
+}
+#add{
+    margin-left: 180px;
+    color:red;
+}
+</style>
