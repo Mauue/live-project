6029eb190a634011a54a5d098cdd0c8d1e75bf76
ViridianFairy <297091829@qq.com>
2020-03-15 15:21:30 +0800

    封装填表

---
+++ frontend/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+   <div id="app">
+      <order>
+         
+      </order>
+   </div>
+</template>
+
+<script>
+import order from "./components/Order.vue";
+export default {
+   name: "App",
+   components: { order }
+};
+</script>
+
+<style>
+body {
+}
+#app {
+}
+button{
+	margin:0 auto;
+	display: block;
+	background-color:rgb(140, 195, 250);
+	border:none;
+	padding:6px 24px;
+	color:white;
+	font-size: 16px;
+	border-radius: 6px;
+	transition: 0.2s all;
+}
+button:hover{
+	background-color:rgb(120, 179, 238);
+}
+</style>
---
+++ frontend/src/components/info.vue
@@ -0,0 +1,110 @@
+<template>
+   <div class="info">
+      <header>个人信息</header>
+      <section v-for="(item,i) in config" :key="item.name">
+         <div>{{item.name}}</div>
+         <div style="color:#ED9E7E" v-if="!inputValid[i]">
+            <img src="../assets/common/t.svg" />
+            {{"请检查您的输入"}}
+         </div>
+         <div style="color:#BFECD3" v-else>
+            <img src="../assets/common/f.svg" />
+            {{"OK"}}
+         </div>
+         <input :placeholder="'请输入'+item.name" @input="checkInput(item,i)" 
+			 v-model="inputText[i]" :key="item.name" :maxlength="item.size[1]">
+      </section>
+   </div>
+</template>
+
+<script>
+export default {
+   name: "info",
+   props: {
+		config:{
+			type:Array
+		}
+	},
+	data(){
+		return{
+			inputText:[],
+			inputValid:[],
+			
+		}
+	},
+	computed:{
+		
+	},
+	methods:{
+		checkInput(item, index){
+			var type = item.type
+			var min = item.size[0]
+			var max = item.size[1]
+			var text = this.inputText[index]
+			var t = false
+			console.log(item)
+			if(type=='digit'){
+				t = /^[0-9]+$/g.test(text) && text.length>=min && text.length<=max
+				this.inputValid[index] = Boolean(t)
+				if(text<item.limit[0] && text) this.inputText[index]=item.limit[0]
+				if(text>item.limit[1] && text) this.inputText[index]=item.limit[1]
+			}
+			if(type=='chn'){
+				t = /^[^\x00-\xff]+$/g.test(text) && text.length>=min && text.length<=max
+				this.inputValid[index] = Boolean(t)
+			}
+			t && this.$emit('callBack',this.inputText)
+		}
+	}
+};
+</script>
+
+
+<style scoped>
+.info {
+}
+header {
+}
+header::before {
+   display: inline-block;
+   height: 16px;
+   width: 4px;
+   content: "";
+   background-color: #1285f8;
+   margin: 0px 10px -3px 0;
+   border-radius: 5px;
+}
+.info section:nth-of-type(1) {
+   margin-top: 10px;
+   border-top: 1px #eee solid;
+}
+section {
+   overflow: hidden;
+   border-bottom: 1px #eee solid;
+   padding: 10px;
+   color: #444;
+}
+section div:nth-child(1),
+section div:nth-child(2) {
+   float: left;
+}
+section div:nth-child(2) {
+   margin-left: 8px;
+   font-size: 14px;
+}
+section div:nth-child(2) img {
+   width: 18px;
+   margin-bottom: -4px;
+   margin-right: 4px;
+}
+section input {
+   border: none;
+   outline: none;
+   font-size: 16px;
+   float: right;
+   text-align: right;
+}
+section input::-webkit-input-placeholder {
+   color: #ccc;
+}
+</style>
