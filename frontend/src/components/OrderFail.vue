b0d253ad389429b0e2249694489dde30b1b95412
HNT <1090687607@qq.com>
2020-03-15 20:36:56 +0800

    增加预约失败界面

---
+++ frontend/src/components/OrderFail.vue
@@ -0,0 +1,20 @@
+<!--未完成-->
+<template>
+   <div id="app">
+		<div id="message">
+            <img src="../assets/cross.svg" />预约失败！
+        </div>
+		<button @click="back" style="margin-top:7px">返回界面</button>
+	</div>
+</template>
+
+
+<style>
+body{
+	
+}
+#message {
+    font-size: 30px;
+    margin: 0 auto;
+}
+</style>
