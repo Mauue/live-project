6acda389bef63d8cc32bb319cbd1cb65d37a2c78
Nicole456 <592056267@qq.com>
2020-03-15 15:06:40 +0800

    update new_order & doc api

--- backend/api/order.py
+++ backend/api/order.py
@@ -21,9 +21,17 @@ def order_info():
 
 def new_order():
     name = request.form.get('name')
+    if name is None:
+        return make_response(msg="名字为空",code=111)
     phone = request.form.get('phone')
+    if phone is None:
+        return make_response(msg="电话号码为空", code=111)
     id_number = request.form.get('id_num')
+    if id_number is None:
+        return make_response(msg="电话号码为空", code=111)
     order_num = request.form.get('order_num')
+    if id_number is None:
+        return make_response(msg="订单数量为空", code=111)
     order_id = "111"
     sql = """INSERT INTO orders(order_id, phone, name, id_number,order_num)
          VALUES (%s, %s, %s, %s,%s)"""
@@ -31,4 +39,5 @@ def new_order():
     db, cursor = get_db()
     cursor.execute(sql, (order_id, phone, name, id_number, order_num))
     cursor.close()
+    return make_response(msg="插入订单项成功")
 
