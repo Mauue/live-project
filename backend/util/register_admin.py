37d53d96399b950e662c4cdd5b68456de5f16eb0
Mauue <364791516@qq.com>
2020-03-15 14:40:52 +0800

    update admin register, api doc

--- backend/util/register_admin.py
+++ backend/util/register_admin.py
@@ -2,9 +2,9 @@ from db.admin import new_admin
 import click
 
 
-@click.command('register-admin')
-@click.option('--username', help="admin username", required=True)
-@click.option('--password', help="admin password", required=True)
+@click.command('register')
+@click.option('-u', help="admin username", required=True)
+@click.option('-p', help="admin password", required=True)
 def register_admin(username, password):
     success, msg = new_admin(username, password)
     if success:
